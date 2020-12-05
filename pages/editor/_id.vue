8a11173871a37fdd7f3b7c059995d44f8da358eb
Tobi406 <tobim406@gmail.com>
2020-12-05 13:54:45 +0100

    Upload example

---
+++ pages/editor/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Id: {{ $route.params.id }}</p>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Editor',
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/editor/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Hi!!</p>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Editor',
+  },
+}
+</script>
+
+<style>
+
+</style>
